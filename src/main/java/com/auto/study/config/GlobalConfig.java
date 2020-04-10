fe48af475528c346ca2470adc7f29d50d6ba19c5
zhanghangtian <zhanghangtian@tuhu.cn>
2020-04-10 18:37:20 +0800

    mod

--- src/main/java/com/auto/study/config/GlobalConfig.java
+++ src/main/java/com/auto/study/config/GlobalConfig.java
@@ -7,13 +7,14 @@ import org.springframework.context.annotation.Configuration;
 public class GlobalConfig {
 
 	public static String port;
-	public static String mainPageUrl = "http://hnpi.newzhihui.cn/";
+	public static String mainPageUrl = "http://47.92.44.63:89/";
 	public static String checkCodeUrl = "http://hnpi.newzhihui.cn/validate.do";
-	public static String loginUrl = "http://hnpi.newzhihui.cn/frontLogin.do";
-	public static String userCenter = "http://hnpi.newzhihui.cn/userCenter.do?action=toLesson";
+	public static String loginUrl = "http://47.92.44.63:89/api/login";
+	public static String userCenter = "http://47.92.44.63:89/api/getlist?page=1&pageSize=200";
 	public static String studyPage = "http://hnpi.newzhihui.cn/";
 	public static String updateItemRate = "http://hnpi.newzhihui.cn/study.do?action=updateItemRate";
 	public static String examPaper = "http://hnpi.newzhihui.cn/examPaper.do?action=intoExamPaper";
+	public static String updateRate = "http://47.92.44.63:89/api/setGiveUpList?id=";
 	
 	@Value("${server.port}")
 	public void setPort(String port) {
--- src/main/java/com/auto/study/service/SystemService.java
+++ src/main/java/com/auto/study/service/SystemService.java
@@ -15,6 +15,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.auto.study.config.GlobalConfig;
 import com.auto.study.domain.Class;
@@ -26,213 +28,182 @@ import com.auto.study.domain.util.ResolveUtil;
 @Service
 public class SystemService {
 
-	private static Logger logger = LoggerFactory.getLogger(SystemService.class);
+    private static Logger       logger      = LoggerFactory.getLogger(SystemService.class);
 
-	@Autowired
-	private ResolveUtil resolveUtil;
+    @Autowired
+    private ResolveUtil         resolveUtil;
 
-	public static List<UserRes> userResList = Collections.synchronizedList(new ArrayList<UserRes>());;
+    public static List<UserRes> userResList = Collections.synchronizedList(new ArrayList<UserRes>());;
 
-	public UserRes initNewClient() throws Exception {
-		UserRes userRes = new UserRes();
-		userRes.setId(generateId());
-		userRes.setLoginState(false);
-		HttpClientUtil httpClientUtil = userRes.getHttpClientUtil();
-		httpClientUtil.sendGetRequestForHtml(GlobalConfig.mainPageUrl);
-		userResList.add(userRes);
-		return userRes;
-	}
+    public UserRes initNewClient() throws Exception {
+        UserRes userRes = new UserRes();
+        userRes.setId(generateId());
+        userRes.setLoginState(false);
+        HttpClientUtil httpClientUtil = userRes.getHttpClientUtil();
+        httpClientUtil.sendGetRequestForHtml(GlobalConfig.mainPageUrl);
+        userResList.add(userRes);
+        return userRes;
+    }
 
-	public void getCheckCode(String id, OutputStream outPutStream) throws Exception {
-		UserRes userRes = getUserResById(id);
-		CloseableHttpResponse response = userRes.getHttpClientUtil()
-				.sendGetRequestForResponse(GlobalConfig.checkCodeUrl);
-		response.getEntity().writeTo(outPutStream);
-		response.close();
-	}
+    public void getCheckCode(String id, OutputStream outPutStream) throws Exception {
+        UserRes userRes = getUserResById(id);
+        CloseableHttpResponse response = userRes.getHttpClientUtil()
+                .sendGetRequestForResponse(GlobalConfig.checkCodeUrl);
+        response.getEntity().writeTo(outPutStream);
+        response.close();
+    }
 
-	public String userLogin(String id, String account, String password, String checkCode) throws Exception {
-		for (UserRes userRes : userResList) {
-			if (userRes.getAccount() != null && userRes.getAccount().equals(account)) {
-				// userDelete(id);
-				return "{\"result\":\"false\",\"language\":\"zh\",\"error\":\"您已登陆请勿重复登陆!\"}";
-			}
-		}
-		UserRes userRes = getUserResById(id);
-		HashMap<String, String> params = new HashMap<>();
-		params.put("loginName", account);
-		params.put("password", password);
-		params.put("roleType", "1");
-		params.put("unittitle", "1");
-		params.put("unit_syscode", "1");
-		params.put("type", "1");
-		params.put("vcode", checkCode);
-		String result = userRes.getHttpClientUtil().sendPostRequestForHtmlWithParam(GlobalConfig.loginUrl, params);
-		JSONObject jsonObject = JSONObject.parseObject(result);
-		String state = jsonObject.get("result").toString();
-		if ("success".equals(state)) {
-			userRes.setAccount(account);
-			userRes.setPassword(password);
-			userRes.setLoginState(true);
-			userRes.setName(jsonObject.get("name").toString());
-			userLoginSucProcess(userRes);
-		}
-		return result;
-	}
+    public String userLogin(String id, String account, String password, String checkCode) throws Exception {
+        for (UserRes userRes : userResList) {
+            if (userRes.getAccount() != null && userRes.getAccount().equals(account)) {
+                // userDelete(id);
+                return "{\"result\":\"false\",\"language\":\"zh\",\"error\":\"您已登陆请勿重复登陆!\"}";
+            }
+        }
+        UserRes userRes = getUserResById(id);
+        HashMap<String, String> params = new HashMap<>();
+        params.put("userName", account);
+        params.put("password", password);
+        String result = userRes.getHttpClientUtil().sendPostRequestForHtmlWithParam(GlobalConfig.loginUrl, params);
+        JSONObject jsonObject = JSONObject.parseObject(result);
+        Boolean state = Boolean.parseBoolean(jsonObject.get("success").toString());
+        if (state) {
+            userRes.setAccount(account);
+            userRes.setPassword(password);
+            userRes.setLoginState(true);
+            //userRes.setName(jsonObject.get("name").toString());
+            userLoginSucProcess(userRes);
+        }
+        return "学习完成";
+    }
 
-	private String generateId() {
-		return UUID.randomUUID().toString().replace("-", "").toUpperCase().substring(0, 5);
-	}
+    private String generateId() {
+        return UUID.randomUUID().toString().replace("-", "").toUpperCase().substring(0, 5);
+    }
 
-	private UserRes getUserResById(String id) throws Exception {
-		for (UserRes userRes : userResList) {
-			if (userRes.getId().equals(id)) {
-				return userRes;
-			}
-		}
-		return null;
-	}
+    private UserRes getUserResById(String id) throws Exception {
+        for (UserRes userRes : userResList) {
+            if (userRes.getId().equals(id)) {
+                return userRes;
+            }
+        }
+        return null;
+    }
 
-//	private void clearUser() {
-//		Iterator<UserRes> it = userResList.iterator();
-//		while (it.hasNext()) {
-//			UserRes x = it.next();
-//			if (!x.isLoginState()) {
-//				if (x.getAccount() == null) {
-//					it.remove();
-//				}
-//			}
-//		}
-//	}
+    //	private void clearUser() {
+    //		Iterator<UserRes> it = userResList.iterator();
+    //		while (it.hasNext()) {
+    //			UserRes x = it.next();
+    //			if (!x.isLoginState()) {
+    //				if (x.getAccount() == null) {
+    //					it.remove();
+    //				}
+    //			}
+    //		}
+    //	}
 
-	public List<UserRes> userList() {
-		return userResList;
-	}
+    public List<UserRes> userList() {
+        return userResList;
+    }
 
-	public void userDelete(String id) {
-		for (UserRes userRes : userResList) {
-			if (userRes.getId().equals(id)) {
-				userResList.remove(userRes);
-				return;
-			}
-		}
-	}
+    public void userDelete(String id) {
+        for (UserRes userRes : userResList) {
+            if (userRes.getId().equals(id)) {
+                userResList.remove(userRes);
+                return;
+            }
+        }
+    }
 
-	private void userLoginSucProcess(UserRes userRes) throws Exception {
-		Project project = new Project();
-		long allTimes = 0L;
-		boolean finished = true;
-		String centerHtml = userRes.getHttpClientUtil().sendGetRequestForHtml(GlobalConfig.userCenter);
-		String stringid = resolveUtil.getElement(centerHtml, "div.item-line a.btn-primary").attr("href");
-		project.setId(stringid);
-		ArrayList<Class> classes = new ArrayList<>();
-		String html;
-		try {
-			html = userRes.getHttpClientUtil().sendGetRequestForHtml(GlobalConfig.studyPage + stringid);
-		} catch (Exception e) {
-			html = userRes.getHttpClientUtil().sendGetRequestForHtml(GlobalConfig.studyPage + stringid);
-			e.printStackTrace();
-		}
-		Elements coruseArrays = resolveUtil.getElementArray(html, "div[du-render='electiveCourse'] div.course-list");
-		for (Element element : coruseArrays) {
-			Elements ulElements = element.select("ul[id]");
-			for (Element ulElement : ulElements) {
-				Element lastUl = ulElement.select("ul[id]").first();
-				Elements lis = lastUl.getElementsByTag("li");
-				for (Element li : lis) {
-					String id = li.attr("id");
-					Class inClass = new Class();
-					inClass.setpId(lastUl.attr("id"));
-					inClass.setId(id);
-					int times = Integer.parseInt(li.getElementById(id + "-times").attr("value"));
-					inClass.setTime(times);
-					inClass.setName(li.attr("data-original-title"));
-					int rate = Integer.parseInt((li.select("span#" + id).html()).replaceAll("%", ""));
-					if (rate >= 100) {
-						inClass.setFinished(true);
-					} else {
-						finished = false;
-					}
-					inClass.setRate(rate);
-					classes.add(inClass);
-					allTimes += times;
-				}
-			}
-		}
-		project.setAllTimes(allTimes);
-		project.setFinished(finished);
-		project.setClasses(classes);
-		if (project.isFinished()) {
-			userRes.setStudyState(2);
-		}
-		userRes.setProject(project);
-	}
+    private void userLoginSucProcess(UserRes userRes) throws Exception {
+        Project project = new Project();
+        long allTimes = 0L;
+        boolean finished = true;
+        String centerHtml = userRes.getHttpClientUtil().sendGetRequestForHtml(GlobalConfig.userCenter);
+        JSONObject classListObject = JSONObject.parseObject(centerHtml);
+        JSONArray classList = classListObject.getJSONArray("list");
+        //OID GiveUp duration 时长
+        for (Object temp : classList) {
+            JSONObject t = JSONObject.parseObject(temp.toString());
+            String OID = t.getString("OID");
+            int duration = t.getInteger("duration");
+            int num = duration / 2 + duration % 2;
+            while (num > 0) {
+                String result = userRes.getHttpClientUtil().sendGetRequestForHtml(GlobalConfig.updateRate + OID);
+                if (result.contains("false")) {
+                    System.out.println("ERROR:" + t.getString("name") + "异常");
+                }
+                num--;
+            }
+            System.out.println(t.getString("name") + "学习完成");
+        }
+    }
 
-	public String startStudy(String id) throws Exception {
-		UserRes userRes = getUserResById(id);
-		if (userRes.getStudyState() == 2) {
-			return "学习已完成";
-		}
-		Project project = userRes.getProject();
-		if (project.isFinished()) {
-			userRes.setStudyState(2);
-			return "学习已完成";
-		}
-		new Thread() {
-			@Override
-			public void run() {
-				mainStudy(userRes);
-			}
-		}.start();
-		userRes.setStudyState(1);
-		return "开始自动学习";
-	}
+    public String startStudy(String id) throws Exception {
+        UserRes userRes = getUserResById(id);
+        if (userRes.getStudyState() == 2) {
+            return "学习已完成";
+        }
+        Project project = userRes.getProject();
+        if (project.isFinished()) {
+            userRes.setStudyState(2);
+            return "学习已完成";
+        }
+        new Thread() {
+            @Override
+            public void run() {
+                mainStudy(userRes);
+            }
+        }.start();
+        userRes.setStudyState(1);
+        return "开始自动学习";
+    }
 
-	private void mainStudy(UserRes userRes) {
-		Project project = userRes.getProject();
-		List<Class> classes = project.getClasses();
-		// long allTimes = project.getAllTimes();
-		for (Class sClass : classes) {
-			if (sClass.isFinished()) {
-				continue;
-			}
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			HashMap<String, String> params = new HashMap<String, String>();
-			params.put("id", sClass.getId());
-			params.put("time", sClass.getTime() + "");
-			params.put("itemRate", 100 + "");
-			params.put("wareRate", 100 + "");
-			try {
-				String response = userRes.getHttpClientUtil()
-						.sendPostRequestForHtmlWithParam(GlobalConfig.updateItemRate, params);
-				JSONObject jsonObject = JSONObject.parseObject(response);
-				String state = jsonObject.get("result").toString();
-				if (!"success".equals(state)) {
-					logger.error(response);
-					logger.error("课程进度更新失败：" + userRes.getName() + sClass.getName());
-				} else {
-					logger.info("课程进度更新成功：" + userRes.getName() + sClass.getName());
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		userRes.setStudyState(0);
-		project.setFinished(false);
-		try {
-			userLoginSucProcess(userRes);
-		} catch (Exception e) {
-			try {
-				userLoginSucProcess(userRes);
-			} catch (Exception e1) {
-				e1.printStackTrace();
-			}
-			e.printStackTrace();
-		}
-	}
+    private void mainStudy(UserRes userRes) {
+        Project project = userRes.getProject();
+        List<Class> classes = project.getClasses();
+        // long allTimes = project.getAllTimes();
+        for (Class sClass : classes) {
+            if (sClass.isFinished()) {
+                continue;
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            HashMap<String, String> params = new HashMap<String, String>();
+            params.put("id", sClass.getId());
+            params.put("time", sClass.getTime() + "");
+            params.put("itemRate", 100 + "");
+            params.put("wareRate", 100 + "");
+            try {
+                String response = userRes.getHttpClientUtil()
+                        .sendPostRequestForHtmlWithParam(GlobalConfig.updateItemRate, params);
+                JSONObject jsonObject = JSONObject.parseObject(response);
+                String state = jsonObject.get("result").toString();
+                if (!"success".equals(state)) {
+                    logger.error(response);
+                    logger.error("课程进度更新失败：" + userRes.getName() + sClass.getName());
+                } else {
+                    logger.info("课程进度更新成功：" + userRes.getName() + sClass.getName());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        userRes.setStudyState(0);
+        project.setFinished(false);
+        try {
+            userLoginSucProcess(userRes);
+        } catch (Exception e) {
+            try {
+                userLoginSucProcess(userRes);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }
 
 }
